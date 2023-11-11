58275e98bf5fdb7a00ba7a5ad88d845fc915694d
free-x <oroitburd@gmail.com>
2023-11-11 08:00:55 +0100

    [WIP] add BMP280

---
+++ lib/iictask/GwBMP280.cpp
@@ -0,0 +1,164 @@
+#include "GwBMP280.h"
+#ifdef _GWIIC
+    #if defined(GWBMP280) || defined(GWBMP28011) || defined(GWBMP28012)|| defined(GWBMP28021)|| defined(GWBMP28022)
+        #define _GWBMP280
+    #else
+        #undef _GWBMP280
+    #endif
+#else
+    #undef _GWBMP280
+    #undef GWBMP280
+    #undef GWBMP28011
+    #undef GWBMP28012
+    #undef GWBMP28021
+    #undef GWBMP28022
+#endif
+#ifdef _GWBMP280
+    #include <Adafruit_BMP280.h>
+#endif
+#ifdef _GWBMP280
+#define PRFX1 "BMP28011"
+#define PRFX2 "BMP28012"
+#define PRFX3 "BMP28021"
+#define PRFX4 "BMP28022"
+class BMP280Config : public SensorBase{
+    public:
+    bool prAct=true;
+    bool tmAct=true;
+    tN2kTempSource tmSrc=tN2kTempSource::N2kts_InsideTemperature;
+    tN2kPressureSource prSrc=tN2kPressureSource::N2kps_Atmospheric;
+    String tmNam="Temperature";
+    String prNam="Pressure";
+    float tmOff=0;
+    float prOff=0;
+    Adafruit_BMP280 *device=nullptr;
+    uint32_t sensorId=-1;
+    BMP280Config(GwApi * api, const String &prfx):SensorBase(api,prfx){
+        }
+    virtual bool isActive(){return prAct||huAct||tmAct;}
+    virtual bool initDevice(GwApi *api,TwoWire *wire){
+        GwLog *logger=api->getLogger();
+        device= new Adafruit_BMP280();
+        if (! device->begin(addr,wire)){
+            LOG_DEBUG(GwLog::ERROR,"unable to initialize %s at %d",prefix.c_str(),addr);
+            delete device;
+            device=nullptr;
+            return false;
+        }
+        sensorId=device->sensorID();
+        LOG_DEBUG(GwLog::LOG, "initialized %s at %d, sensorId 0x%x", prefix.c_str(), addr, sensorId);
+        return (sensorId == 0x56 || sensorId == 0x57 || sensorId == 0x58) || tmAct || prAct;
+    }
+    virtual bool preinit(GwApi * api){
+        GwLog *logger=api->getLogger();
+        LOG_DEBUG(GwLog::LOG,"%s configured",prefix.c_str());
+        api->addCapability(prefix,"true");
+        addPressureXdr(api,*this);
+        addTempXdr(api,*this);
+        return isActive();
+    }
+    virtual void measure(GwApi *api, TwoWire *wire, int counterId)
+    {
+        if (!device)
+            return;
+        GwLog *logger = api->getLogger();
+        if (prAct)
+        {
+            float pressure = device->readPressure();
+            float computed = pressure + prOff;
+            LOG_DEBUG(GwLog::DEBUG, "%s measure %2.0fPa, computed %2.0fPa", prefix.c_str(), pressure, computed);
+            sendN2kPressure(api, *this, computed, counterId);
+        }
+        if (tmAct)
+        {
+            float temperature = device->readTemperature(); // offset is handled internally
+            temperature = CToKelvin(temperature);
+            LOG_DEBUG(GwLog::DEBUG, "%s measure temp=%2.1f", prefix.c_str(), temperature);
+            sendN2kTemperature(api, *this, temperature, counterId);
+        }
+    }
+    #define CFG280(prefix) \
+        CFG_GET(prAct,prefix); \
+        CFG_GET(tmAct,prefix);\
+        CFG_GET(tmSrc,prefix);\
+        CFG_GET(iid,prefix);\
+        CFG_GET(intv,prefix);\
+        CFG_GET(tmNam,prefix);\
+        CFG_GET(prNam,prefix);\
+        CFG_GET(tmOff,prefix);\
+        CFG_GET(prOff,prefix);
+
+    virtual void readConfig(GwConfigHandler *cfg) override
+    {
+        if (prefix == PRFX1)
+        {
+            busId = 1;
+            addr = 0x76;
+            CFG280(BMP28011);
+            ok=true;
+        }
+        if (prefix == PRFX2)
+        {
+            busId = 1;
+            addr = 0x77;
+            CFG280(BMP28012);
+            ok=true;
+        }
+        if (prefix == PRFX3)
+        {
+            busId = 2;
+            addr = 0x76;
+            CFG280(BMP28021);
+            ok=true;
+        }
+        if (prefix == PRFX4)
+        {
+            busId = 2;
+            addr = 0x77;
+            CFG280(BMP28022);
+        }
+        intv *= 1000;
+    }
+};
+
+
+void registerBMP280(GwApi *api,SensorList &sensors){
+    #if defined(GWBMP280) || defined(GWBMP28011)
+    {
+        BMP280Config *cfg=new BMP280Config(api,PRFX1);
+        sensors.add(api,cfg);
+        CHECK_IIC1();
+        #pragma message "GWBMP28011 defined"
+    }
+    #endif
+    #if defined(GWBMP28012)
+    {
+        BMP280Config *cfg=new BMP280Config(api,PRFX2);
+        sensors.add(api,cfg);
+        CHECK_IIC1();
+        #pragma message "GWBMP28012 defined"
+    }
+    #endif
+    #if defined(GWBMP28021)
+    {
+        BMP280Config *cfg=new BMP280Config(api,PRFX3);
+        sensors.add(api,cfg);
+        CHECK_IIC2();
+        #pragma message "GWBMP28021 defined"
+    }
+    #endif
+    #if defined(GWBMP28022)
+    {
+        BMP280Config *cfg=new BMP280Config(api,PRFX4);
+        sensors.add(api,cfg);
+        CHECK_IIC1();
+        #pragma message "GWBMP28022 defined"
+    }
+    #endif
+}
+#else
+void registerBMP280(GwApi *api,SensorList &sensors){   
+}
+
+#endif
+
---
+++ lib/iictask/GwBMP280.h
@@ -0,0 +1,5 @@
+#ifndef _GWBMP280_H
+#define _GWBMP280_H
+#include "GwIicSensors.h"
+void registerBMP280(GwApi *api,SensorList &sensors);
+#endif
